01d992e035aa18b8ec59ea40df0cc1af14249ca5
AleksandrZachkevich <aleksdark13@gmail.com>
2023-11-28 08:07:56 +0200

    Initial build subtree commit

---
+++ webpack.prod.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+const CopyPlugin = require("copy-webpack-plugin");
+
+module.exports = {
+    mode: 'production',
+    entry: path.resolve(__dirname, 'src', 'index.tsx'),
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+    },
+};
